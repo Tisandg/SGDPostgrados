317d60bcda5de7b4bed9f685759b95c476d8bf6a
William Romero <LENOVO@192.168.1.12>
2017-09-18 15:14:07 -0500

    correo obligatorio, redacción correo en registro de Est, avisos de error de autenticación ¡ojo! en inicio de sesisón no se debe alertar sobre usuario o contraseña obligatorio, solo se muestra el error de autenticación

---
+++ src/java/co/unicauca/proyectobase/validadores/ValidadorUsuario.java
@@ -0,0 +1,27 @@
+package co.unicauca.proyectobase.validadores;
+
+import co.unicauca.proyectobase.dao.EstudianteFacade;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+@FacesValidator(value="validadorUsuario")
+
+public class ValidadorUsuario implements Validator{
+     @EJB
+     @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        String usuario = String.valueOf(value);
+
+        if(usuario.length() == 0) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "El usuario  es obligatorio.");
+            throw new ValidatorException(msg);  
+        }
+    }
+}
