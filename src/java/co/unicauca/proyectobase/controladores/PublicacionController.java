d22c5a600d4fbf0403f1f831db093113898ccdda
santilopez94 <santiagolopez94@unicauca.edu.co>
2017-09-25 09:30:46 -0500

    Creacion pdf

--- src/java/co/unicauca/proyectobase/controladores/PublicacionController.java
+++ src/java/co/unicauca/proyectobase/controladores/PublicacionController.java
@@ -14,14 +14,28 @@ import co.unicauca.proyectobase.entidades.CapituloLibro;
 //import static co.unicauca.proyectobase.entidades.GrupoTipoUsuario_.nombreUsuario;
 import co.unicauca.proyectobase.entidades.archivoPDF;
 import co.unicauca.proyectobase.utilidades.Utilidades;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.openkm.sdk4j.exception.AccessDeniedException;
 import com.openkm.sdk4j.exception.PathNotFoundException;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
@@ -40,7 +54,9 @@ import javax.ejb.EJBException;
 import java.util.Base64;*/
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
@@ -83,6 +99,11 @@ public class PublicacionController implements Serializable {
     
     private CargarVistaEstudiante cve;
     private CargarVistaCoordinador cvc;
+    
+    ArrayList<Publicacion> rev;
+    ArrayList<Publicacion> lib;
+    ArrayList<Publicacion> con;
+    ArrayList<Publicacion> cap;
 
     public String getMotRechazo() {
         return motRechazo;
@@ -218,28 +239,149 @@ public class PublicacionController implements Serializable {
         }
 
     }
-
-    public List<Publicacion> listadoPublicaciones(String nombreUsuario) {
-
-        if ((variableFiltrado == null) || (variableFiltrado.equals(""))) {
-
-            Estudiante est = dao.obtenerEstudiante(nombreUsuario);
-            setAuxEstudiante(est);
-
-            int idEstudiante = est.getEstIdentificador();
-            return dao.ListadoPublicacionEst(idEstudiante);
-
-        } else {
-
-            Estudiante est = dao.obtenerEstudiante(nombreUsuario);
-            setAuxEstudiante(est);
-
-            int idEstudiante = est.getEstIdentificador();
-            return dao.ListadoPublicacionEstFilt(idEstudiante, variableFiltrado);
-
+    public void llenarListas()
+    {
+        rev= new ArrayList();
+        lib= new ArrayList();
+        con= new ArrayList();
+        cap= new ArrayList();
+        
+        for (int i = 0; i < dao.findAll().size(); i++) {
+            if(dao.findAll().get(i).getPubTipoPublicacion().equals("revista"))
+                 rev.add(dao.findAll().get(i));
+            if(dao.findAll().get(i).getPubTipoPublicacion().equals("libro"))
+                 lib.add(dao.findAll().get(i));
+            if(dao.findAll().get(i).getPubTipoPublicacion().equals("congreso"))
+                 con.add(dao.findAll().get(i));
+            if(dao.findAll().get(i).getPubTipoPublicacion().equals("capitulo_libro"))
+                 cap.add(dao.findAll().get(i));
         }
-
+        
     }
+        
+        
+    
+    
+    public List<Publicacion> listadoPdf() throws FileNotFoundException, IOException {
+       
+        llenarListas();
+        Document document= new Document();
+        String realPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+        System.out.println("path"+ realPath); 
+        String conso = realPath + "resources\\pdf\\" + "Consolidado" + ".pdf";
+        FileOutputStream archivo = new FileOutputStream(conso);
+        try{
+         PdfWriter.getInstance(document, archivo);
+         document.open();
+         document.add(new Paragraph("Consolidado de Publicaciones\n"));
+         DateFormat formatter= new SimpleDateFormat("dd/MM/yy '-' hh:mm:ss");
+         Date currentDate = new Date();
+         String date = formatter.format(currentDate);
+         document.add(new Paragraph("Fecha Generado: "+date)); 
+         document.add(new Paragraph("\n"));
+         PdfPTable table = new PdfPTable(4);
+         table.setTotalWidth(new float[]{ 70,72, 110, 95});
+         table.setLockedWidth(true);
+         table.addCell("Nombre de Autor");
+         table.addCell("Nombre Publicacion");
+         table.addCell("Estado de la Publicacion");
+         table.addCell("Fecha de Registro");
+         
+         PdfPCell revista = new PdfPCell(new Paragraph("Revista" ,
+                       FontFactory.getFont("arial",   // fuente
+                         8,                            // tamaño
+                     Font.BOLD,                   // estilo
+                               BaseColor.WHITE)));
+                  revista.setHorizontalAlignment(Element.ALIGN_CENTER);
+                  revista.setBackgroundColor(BaseColor.GRAY);
+                  revista.setColspan(9);
+                  table.addCell(revista);
+         
+         for (int i = 0; i < rev.size(); i++) {
+             
+                        
+            table.addCell(rev.get(i).getPubNombreAutor());
+            table.addCell(rev.get(i).obtenerNombrePub());
+            table.addCell(rev.get(i).getPubEstado());
+            table.addCell(rev.get(i).getPubFechaPublicacion().toString());
+                        
+           }
+         
+       PdfPCell libro = new PdfPCell(new Paragraph("Libro" ,
+                       FontFactory.getFont("arial",   // fuente
+                         8,                            // tamaño
+                     Font.BOLD,                   // estilo
+                               BaseColor.WHITE)));
+                  libro.setHorizontalAlignment(Element.ALIGN_CENTER);
+                  libro.setBackgroundColor(BaseColor.GRAY);
+                  libro.setColspan(9);
+                  table.addCell(libro);
+
+           for (int i = 0; i < lib.size(); i++) {
+                        
+            table.addCell(lib.get(i).getPubNombreAutor());
+            table.addCell(lib.get(i).obtenerNombrePub());
+            table.addCell(lib.get(i).getPubEstado());
+            table.addCell(lib.get(i).getPubFechaPublicacion().toString());
+                        
+           }
+           
+           PdfPCell congreso = new PdfPCell(new Paragraph("Congreso" ,
+                       FontFactory.getFont("arial",   // fuente
+                         8,                            // tamaño
+                     Font.BOLD,                   // estilo
+                               BaseColor.WHITE)));
+                  congreso.setHorizontalAlignment(Element.ALIGN_CENTER);
+                  congreso.setBackgroundColor(BaseColor.GRAY);
+                  congreso.setColspan(9);
+                  table.addCell(congreso);
+                  
+                   for (int i = 0; i < con.size(); i++) {
+                        
+            table.addCell(con.get(i).getPubNombreAutor());
+            table.addCell(con.get(i).obtenerNombrePub());
+            table.addCell(con.get(i).getPubEstado());
+            table.addCell(con.get(i).getPubFechaPublicacion().toString());
+                        
+           }
+                    PdfPCell capitulo = new PdfPCell(new Paragraph("Capitulo Libro" ,
+                       FontFactory.getFont("arial",   // fuente
+                         8,                            // tamaño
+                     Font.BOLD,                   // estilo
+                               BaseColor.WHITE)));
+                  capitulo.setHorizontalAlignment(Element.ALIGN_CENTER);
+                  capitulo.setBackgroundColor(BaseColor.GRAY);
+                  capitulo.setColspan(9);
+                  table.addCell(capitulo);
+
+           for (int i = 0; i < cap.size(); i++) {
+                        
+            table.addCell(cap.get(i).getPubNombreAutor());
+            table.addCell(cap.get(i).obtenerNombrePub());
+            table.addCell(cap.get(i).getPubEstado());
+            table.addCell(cap.get(i).getPubFechaPublicacion().toString());
+                        
+           }
+        
+          document.add(table);
+           
+        
+           }catch(Exception ex)
+           {
+            System.out.println("Error " + ex.getMessage());
+           }
+          document.close();
+          
+     
+          
+       
+            
+       
+
+            return dao.findAll();
+    }
+    
+   
 
     public List<Publicacion> listadoEspera() {
 
@@ -262,6 +404,27 @@ public class PublicacionController implements Serializable {
             return listaPublicacionVisadoRevisada(dao.ListadoPublicacionFilt(variableFiltrado));
         }
     }
+    
+    public List<Publicacion> listadoPublicaciones(String nombreUsuario) {
+
+        if ((variableFiltrado == null) || (variableFiltrado.equals(""))) {
+
+            Estudiante est = dao.obtenerEstudiante(nombreUsuario);
+            setAuxEstudiante(est);
+            int idEstudiante = est.getEstIdentificador();
+            return dao.ListadoPublicacionEst(idEstudiante);
+
+        } else {
+
+            Estudiante est = dao.obtenerEstudiante(nombreUsuario);
+            setAuxEstudiante(est);
+            int idEstudiante = est.getEstIdentificador();
+            return dao.ListadoPublicacionEstFilt(idEstudiante, variableFiltrado);
+
+        }
+
+    }
+    
 
     /* Lista las publicaciones que su estado de Visado sea: espera,
      es decir publicaciones que aun no han sido visadas*/
