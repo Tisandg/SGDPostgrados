a13bf53a4a6f69c45d161ed83cd1b19c682f0e55
Carolina <ingrithc@unicauca.edu.co>
2017-12-07 14:45:10 -0500

    Documentación de utilidades

--- src/java/co/unicauca/proyectobase/utilidades/PropiedadesOS.java
+++ src/java/co/unicauca/proyectobase/utilidades/PropiedadesOS.java
@@ -1,12 +1,16 @@
 package co.unicauca.proyectobase.utilidades;
 
 /**
- * Clase encargada de retornar el separdor utilizado en cada sistema operativo.
+ * Clase encargada de retornar el separador utilizado en cada sistema operativo.
  * Por separador se entiende el caracter de navegación entre carpetas.
  * @author Danilo López - dlopezs@unicauca.edu.co
  */
 public class PropiedadesOS {
     
+    /**
+     * Método que retorna el separador utilizado en cada sistema operativo.
+     * @return separador o caracter de navegación entre carpetas.
+     */
     public String getSeparator()
     {
         return System.getProperty("file.separator");
