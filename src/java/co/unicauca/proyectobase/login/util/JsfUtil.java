a136e929ea171aa6bb5b222d5a1797c8bf0274a3
Carolina <ingrithc@unicauca.edu.co>
2017-12-07 15:29:32 -0500

    Documentación de los util de login

--- src/java/co/unicauca/proyectobase/login/util/JsfUtil.java
+++ src/java/co/unicauca/proyectobase/login/util/JsfUtil.java
@@ -9,6 +9,12 @@ import javax.faces.model.SelectItem;
 
 public class JsfUtil {
 
+    /**
+     * Método que permite obtener los items seleccionados.
+     * @param entities: Lista de entidades.
+     * @param selectOne: indicador de si esta seleccionado un elemento
+     * @return items seleccionados.
+     */
     public static SelectItem[] getSelectItems(List<?> entities, boolean selectOne) {
         int size = selectOne ? entities.size() + 1 : entities.size();
         SelectItem[] items = new SelectItem[size];
@@ -22,11 +28,19 @@ public class JsfUtil {
         }
         return items;
     }
-
+/**
+     * Método que permite obtener el resultado de la validación.
+     * @return true si fallo y false en caso contrario.
+     */
     public static boolean isValidationFailed() {
         return FacesContext.getCurrentInstance().isValidationFailed();
     }
 
+    /**
+     * Método que permite agregar un mensaje de error.
+     * @param ex: excepción.
+     * @param defaultMsg: mensaje perteneciente a la excepción.
+     */
     public static void addErrorMessage(Exception ex, String defaultMsg) {
         String msg = ex.getLocalizedMessage();
         if (msg != null && msg.length() > 0) {
@@ -36,31 +50,58 @@ public class JsfUtil {
         }
     }
 
+    /**
+     * Método que permite agregar un mensaje de error a una lista de mensajes de error.
+     * @param messages: mensajes de error.
+     */
     public static void addErrorMessages(List<String> messages) {
         for (String message : messages) {
             addErrorMessage(message);
         }
     }
 
+     /**
+     * Método que permite agregar un mensaje de error.
+     * @param msg: mensaje de error.
+     */
     public static void addErrorMessage(String msg) {
         FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
         FacesContext.getCurrentInstance().addMessage(null, facesMsg);
     }
 
+   /**
+     * Método que permite agregar un mensaje de éxito.
+     * @param msg: mensaje de éxito.
+     */
     public static void addSuccessMessage(String msg) {
         FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg);
         FacesContext.getCurrentInstance().addMessage("successInfo", facesMsg);
     }
 
+    /**
+     * Método que permite obtener un parámetro de solicitud.
+     * @param key: clave.
+     * @return cadena con los parametros.
+     */
     public static String getRequestParameter(String key) {
         return FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get(key);
     }
 
+    /**
+     * Método que permite obtener un objeto del parámetro de solicitud
+     * @param requestParameterName: nombre del parámetro de solicitud
+     * @param converter: objeto de tipo converter
+     * @param component: componente GUI
+     * @return objeto del parámetro de solicitud
+     */
     public static Object getObjectFromRequestParameter(String requestParameterName, Converter converter, UIComponent component) {
         String theId = JsfUtil.getRequestParameter(requestParameterName);
         return converter.getAsObject(FacesContext.getCurrentInstance(), component, theId);
     }
 
+    /**
+     * Ennumeración con las acciones de persistencia.
+     */
     public static enum PersistAction {
         CREATE,
         DELETE,
